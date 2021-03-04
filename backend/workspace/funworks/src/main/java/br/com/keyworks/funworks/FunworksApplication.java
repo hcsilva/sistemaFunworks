d0e24af5e50e72a7dd340d2b99579bd1b0f62070
hcsilva <henriquec.dasilva@hotmail.com>
2021-03-03 21:15:09 -0300

    Estrutura Inicial do BackEnd, iniciando o projeto

---
+++ backend/workspace/funworks/src/main/java/br/com/keyworks/funworks/FunworksApplication.java
@@ -0,0 +1,13 @@
+package br.com.keyworks.funworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunworksApplication.class, args);
+	}
+
+}
